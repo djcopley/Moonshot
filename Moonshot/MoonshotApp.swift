1c0ea9c765230f0579b03ae076caf42f262db0eb
Daniel Copley <djcopley@users.noreply.github.com>
2022-12-01 21:08:49 -0500

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Daniel Copley on 12/1/22.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
